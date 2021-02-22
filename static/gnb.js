802d1c21306f562dfad4bc3f812167def709ee98
DESKTOP-IBOIH3A\edge_mjh <clearm81@naver.com>
2021-02-22 11:57:47 +0900

    정상 모양새 만들기

---
+++ static/gnb.js
@@ -0,0 +1,20 @@
+function fn_gnb()
+{
+    console.log("start");
+
+    let gnb = document.querySelector("#gnb_menu");
+
+    if(gnb == null){
+        console.log("Make tag div gnb_menu ");
+    }
+
+    let menus = '<ul>';
+    menus +='<li><a href="html_1.html"> HTML </a></li><li> <a href="webapi_1.html"> Web API </a> </li>';
+    menus +='<li><a href="css_list.html"> CSS </a> </li>';
+    menus +='<li><a href="javascript:alert(\'준비중\');"> My Post </a> </li>';
+    menus +='</ul>';
+
+    gnb.innerHTML = menus;
+}
+
+fn_gnb();
