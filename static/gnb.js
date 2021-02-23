8e6708292fdb34874a5b747312fedb20fb297ad8
DESKTOP-IBOIH3A\edge_mjh <clearm81@naver.com>
2021-02-23 12:18:21 +0900

    전체다듬기

--- static/gnb.js
+++ static/gnb.js
@@ -8,10 +8,17 @@ function fn_gnb()
         console.log("Make tag div gnb_menu ");
     }
 
+    // let menus = '<ul>';
+    // menus +='<li><a href="html_1.html"> HTML </a></li><li> <a href="webapi_1.html"> Web API </a> </li>';
+    // menus +='<li><a href="css_list.html"> CSS </a> </li>';
+    // menus +='<li><a href="javascript:alert(\'준비중\');"> My Post </a> </li>';
+    // menus +='</ul>';
+
     let menus = '<ul>';
-    menus +='<li><a href="html_1.html"> HTML </a></li><li> <a href="webapi_1.html"> Web API </a> </li>';
-    menus +='<li><a href="css_list.html"> CSS </a> </li>';
-    menus +='<li><a href="javascript:alert(\'준비중\');"> My Post </a> </li>';
+    menus +='<li><a href="profile.html"> About me </a></li>';
+    menus +='<li> <a href="charactor.html"> 성격 </a> </li>';
+    // menus +='<li><a href="css_list.html"> CSS </a> </li>';
+    // menus +='<li><a href="javascript:alert(\'준비중\');"> My Post </a> </li>';
     menus +='</ul>';
 
     gnb.innerHTML = menus;
